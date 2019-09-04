ca70fa9ad7983d6f30695573d778f43cf47495a8
maxime blanc <maxime.blanc@deindeal.ch>
2019-09-04 09:42:59 +0200

    next: use the compose plugin for next config

--- next/next.config.js
+++ next/next.config.js
@@ -1,12 +1,14 @@
+const withPlugins = require('next-compose-plugins');
 const withSass = require('@zeit/next-sass')
 const withTM = require('next-transpile-modules');
 
-module.exports = withTM({
-  transpileModules: ['@smaug/storybook-react'],
-  ...withSass({
+module.exports = withPlugins([
+  [withTM, { transpileModules: ['@smaug/storybook-react'] }],
+  [withSass, {
     cssModules: true,
     cssLoaderOptions: {
         importLoaders: 1,
         localIdentName: "[local]___[hash:base64:5]",
       }
-})})
+  }]
+])

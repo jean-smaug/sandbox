1cc0f59ec35ab62809c31e735b172ad1b9213698
maxime blanc <maxime.blanc@deindeal.ch>
2019-09-04 09:23:26 +0200

    next: remove useless .next and test component from storybook

--- next/next.config.js
+++ next/next.config.js
@@ -1,8 +1,8 @@
 const withSass = require('@zeit/next-sass')
 const withTM = require('next-transpile-modules');
-module.exports = 
-withTM({
-  transpileModules: ['super-saiyan'],
+
+module.exports = withTM({
+  transpileModules: ['@smaug/storybook-react'],
   ...withSass({
     cssModules: true,
     cssLoaderOptions: {
--- next/pages/index.js
+++ next/pages/index.js
@@ -1,8 +1,10 @@
 import css from "../styles/index.scss"
+import { Title } from '@smaug/storybook-react'
 
 function Home() {
     return (
-        <div>Welcome to Next.js!
+        <div>
+            <Title>Welcome to Next.js!</Title>
             <div className={css.example}>Hello World!</div>
         </div>
     )

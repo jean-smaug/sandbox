98b0d34f742107799f80970eea684c65e40e9627
maxime blanc <maxime.blanc@deindeal.ch>
2019-09-06 14:26:54 +0200

    next: apply transform module at the end

--- next/next.config.js
+++ next/next.config.js
@@ -3,12 +3,12 @@ const withSass = require('@zeit/next-sass')
 const withTM = require('next-transpile-modules');
 
 module.exports = withPlugins([
-  [withTM, { transpileModules: ['@smaug/storybook-react'] }],
   [withSass, {
     cssModules: true,
     cssLoaderOptions: {
-        importLoaders: 1,
-        localIdentName: "[local]___[hash:base64:5]",
-      }
-  }]
+      importLoaders: 1,
+      localIdentName: "[local]___[hash:base64:5]",
+    }
+  }],
+  [withTM, { transpileModules: ['@smaug/storybook-react'] }],
 ])

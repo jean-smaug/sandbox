cf8f06e470d304ff3de68f47863ef337cc6e8851
maxime blanc <maxime.blanc@deindeal.ch>
2019-09-17 11:31:46 +0200

    dredd: test api with api blueprint

--- dredd/server.js
+++ dredd/server.js
@@ -4,18 +4,19 @@ const app = express()
 
 const users = [{
     id: 1,
-    name: "Jean Smaug"
+    name: 'Jean Smaug'
 }, {
     id: 2,
-    name: "Timmy"
+    name: 'Timmy'
 }]
 
 app.get('/', function(req, res) {
-    res.json({message: 'Hello World!'});
+    res.json({ message: 'Hello World!' })
 })
   
 app.get('/bogass/:id', function(req, res) {
-    res.json({ name: users[req.params.id] });
+    console.log(typeof req.params.id)
+    res.json(users.find(user => user.id === Number(req.params.id)))
 })
 
 app.listen(3000, () => {
